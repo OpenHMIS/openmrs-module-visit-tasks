7121bc2d3b58f70fdd61d0d57306db0d651ea384
Wes <wes@weslandia.org>
2017-02-23 15:58:36 +0300

    Revert "Code review"

--- omod/src/main/webapp/resources/scripts/constants.js
+++
@@ -1,9 +0,0 @@
-var VISIT_TASK_LANDING_PAGE_URL = ROOT_URL + 'visittasks/visitTasksLanding.page';
-var RELATIVE_CANCEL_PAGE_URL = 'entities.page';
-var VISIT_TASKS_MODULE_NAME = 'visittasks';
-var INVENTORY_TASK_DASHBOARD_PAGE_URL = ROOT_URL + 'openhmis.inventory/inventory/inventoryTasksDashboard.page';
-
-/*Defining the privileges to be used in the angular files*/
-var TASK_MANAGE_METADATA = "Task: Manage Visit Task Metadata";
-var TASK_VIEW_MY_PREDEFINED_TASKS = "Task: View My Predefined Tasks";
-var TASK_MANAGE_VISIT_TASK_METADATA = "Task: Manage Visit Task Metadata";
